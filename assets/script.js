e08f2656a4095d8ed32e521eac88a72a2fb14701
romainBeauger <romain.beauger@gmail.com>
2025-05-12 13:19:56 +0200

    - Ajout du slider complet

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,79 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		image: "slide1.jpg",
+		tagLine: "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		image: "slide2.jpg",
+		tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		image: "slide3.jpg",
+		tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		image: "slide4.png",
+		tagLine: "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+let index = 0;
+
+const bannerImg = document.querySelector(".banner-img");
+const bannerText = document.querySelector(".banner-text");
+const leftArrow = document.getElementById("left");
+const rightArrow = document.getElementById("right");
+const dotsContainer = document.querySelector(".dots");
+
+
+
+function updateSlide() {
+
+    // bannerImg.style.opacity = 0.1;
+
+    // setTimeout(() => {
+        bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+        bannerText.innerHTML = slides[index].tagLine;
+    
+        // Met à jour les points
+        document.querySelectorAll(".dot").forEach((dot, i) => {
+            dot.classList.toggle("active", i === index);
+        });
+        // bannerImg.style.opacity = 1;
+    // }, 300);
+}
+
+rightArrow.addEventListener("click", () => {
+    index = (index + 1) % slides.length;
+    // console.log(index)
+    updateSlide();
+});
+
+leftArrow.addEventListener("click", () => {
+    index = (index - 1 + slides.length) % slides.length;
+    // console.log(index)
+    updateSlide();
+});
+
+// Création des points
+slides.forEach((_, i) => {
+    const dot = document.createElement("span");
+    dot.classList.add("dot");
+    if (i === 0) dot.classList.add("active");
+    dot.dataset.index = i;
+    dot.addEventListener("click", (e) => {
+        index = parseInt(e.target.dataset.index);
+    console.log(index)
+
+        updateSlide();
+    });
+    dotsContainer.appendChild(dot);
+});
+
+
+
+// setInterval(() => {
+//     index = (index + 1) % slides.length;
+//     updateSlide();
+// }, 2500); 
